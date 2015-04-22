fdeeb62c3cd1e33157bccfc8f65d770148cb0f3c
Eniko Pianovszky <eniko.pianovszky@gmail.com>
2015-04-22 12:22:31 +0200

    My Resume
    Front End Nanodegree Project 2 - My Resume web site

--- js/helper.js
+++ js/helper.js
@@ -12,28 +12,34 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
 
+
+
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span id="role">%data%</span>';
+var internationalizeButton = '<span><button type="button" id="btnIntl" class="btn btn-info">Internationalize</button></span>';
+var printButton = '<button type="button" id="btnPrint" class="btn btn-info" onclick="resumePrint()"><span class="glyphicon glyphicon-print"></span> Print</button><hr/>';
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span><a href="#mapDiv" title="See on the map" class="white-text" style="display:inline">%data%</span></a></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img src="%data%" class="biopic" alt="My Photo">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skillsH3">   Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLNavStart = '<ul id="navButtons" class="nav__list"></ul>';
+var HTMLnavButton = '<li class="flex-item" id="nav__item"><span button type="button" class="btn btn-info">%data%</button></span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkLocation = '<div><span><a href="#mapDiv" title="See on the map" class="location-text" style="display:inline">%data%</span></a></div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
@@ -46,7 +52,7 @@ var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolLocation = '<div><span><a href="#mapDiv" title="See on the map" class="location-text" style="display:inline">%data%</span></a></div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
@@ -55,7 +61,7 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+var HTMLgoToTop = '<span><a id="goTo" href="#header">Go to Top</span></a>';
 var googleMap = '<div id="map"></div>';
 
 
@@ -63,11 +69,42 @@ var googleMap = '<div id="map"></div>';
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
 $(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+  $('#btnIntl').click(function() {
+      var status = $('#btnIntl').text();
+      if (status === "Internationalize") {
+        var iName = inName() || function(){};
+        $('#name').html(iName); 
+        $('#btnIntl').text("Back");  
+      } else {
+        $('#name').html(bio.name);
+        $('#btnIntl').text("Internationalize");
+      }
+     
   });
 });
+
+/*XRef to http://stackoverflow.com/questions/16154650/how-to-print-a-hidden-div-when-printing-using-a-javascript-function
+ */
+function resumePrint () {
+  var printContent = document.getElementById('main');
+  var windowUrl = "about:blank";
+  var windowName = "Print";
+  var printWindow = window.open(windowUrl, windowName);
+  printWindow.document.write(printContent.innerHTML);    
+  printWindow.document.getElementById('workExperience').style.display = 'block';
+  printWindow.document.getElementById('projects').style.display = 'block';
+  printWindow.document.getElementById('education').style.display = 'block';
+  printWindow.document.getElementById('nav').style.display = 'none';
+  printWindow.document.getElementById('btnIntl').style.display = 'none';  
+  printWindow.document.getElementById('btnPrint').style.display = 'none';
+  printWindow.document.getElementById('mapDiv').style.display = 'none';
+  printWindow.document.getElementById('letsConnect').style.display = 'none';  
+  printWindow.document.close();
+  printWindow.focus();
+  printWindow.print();
+  printWindow.close();
+  return false;
+};
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
@@ -85,9 +122,10 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+    var x = loc.pageX;
+    var y = loc.pageY;
+    logClicks(x,y);
 });
-
 
 
 /*
@@ -111,7 +149,7 @@ function initializeMap() {
 
   // This next line makes `map` a new Google Map JavaScript Object and attaches it to
   // <div id="map">, which is appended as part of an exercise late in the course.
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+ map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
@@ -170,8 +208,15 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+  service.getDetails(place, function(result, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+      }
+      infoWindow.setContent(result.name);
+      infoWindow.open(map, marker);
     });
+  });
 
     // this is where the pin actually gets added to the map.
     // bounds.extend() takes in a map location object
@@ -185,17 +230,28 @@ function initializeMap() {
   /*
   callback(results, status) makes sure the search returned results for a location.
   If so, it creates a new map marker for that location.
-  */
+ */
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
     }
   }
-
+/*
+function callback(results, status) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    alert(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    createMapMarker(result);
+  }
+};*/    
+    
   /*
   pinPoster(locations) takes in the array of locations created by locationFinder()
   and fires off Google place searches for each location
   */
+   
   function pinPoster(locations) {
 
     // creates a Google place search service object. PlacesService does the work of
@@ -215,6 +271,7 @@ function initializeMap() {
       service.textSearch(request, callback);
     }
   }
+  
 
   // Sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
@@ -233,11 +290,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+// Make sure the map bounds get updated on page resize
+ map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,305 @@
+var bio = {
+    "name" : "Eniko Pianovszky",
+    "role" : "web developer",
+    "contacts" : {
+        "mobile" : "+33 640 31 03 17 97",
+        "email" : "eniko.pianovszky@gmail.com",
+        "github" : "pianove",
+        "twitter" : "N/A",
+        "location" : "Roanne, France"
+    },
+    
+    "biopic" : "images/eniko.jpg",
+    "welcomeMessage" : "Hello, Thank You for reading my bio.",
+    "skills" : ["Creative", "Teaching", "Multi-lingual","Volunteering", "Mother of three"],
+
+    "display" : function () {
+        var formattedName =
+        HTMLheaderName.replace("%data%", bio.name);
+
+        var formattedRole =
+        HTMLheaderRole.replace("%data%", bio.role);
+
+        var formattedPic =
+        HTMLbioPic.replace("%data%", bio.biopic);
+
+        var formattedMobile =
+        HTMLmobile.replace("%data%", bio.contacts.mobile);
+
+        var formattedEmail =
+        HTMLemail.replace("%data%", bio.contacts.email);
+
+        var formattedGithub =
+        HTMLgithub.replace("%data%", bio.contacts.github);
+
+        var formattedTwitter =
+        HTMLtwitter.replace("%data%", bio.contacts.twitter);
+
+        var formattedLocation =
+        HTMLlocation.replace("%data%", bio.contacts.location);
+
+        var formattedWelcomeMsg =
+        HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        
+        $("#header").prepend(printButton);
+        $("#header").prepend(internationalizeButton);
+        $("#header").prepend(formattedRole);
+        $("#header").prepend(formattedName);
+        $("#header").append(formattedPic);
+        $("#topContacts").append(formattedMobile);
+        $("#topContacts").append(formattedEmail);
+        $("#topContacts").append(formattedGithub);
+        $("#topContacts").append(formattedTwitter);
+        $("#topContacts").append(formattedLocation);
+        
+        $('#footerContacts').append(formattedMobile);
+        $("#footerContacts").append(formattedEmail);
+        $("#footerContacts").append(formattedGithub);
+        $("#footerContacts").append(formattedTwitter);
+        $("#footerContacts").append(formattedLocation);
+    
+        $("#header").append(formattedWelcomeMsg);
+
+        if (bio.skills.length > 0)
+        {
+            $("#header").append(HTMLskillsStart);
+            for (skill in bio.skills) 
+            {
+                var formattedSkill =
+                HTMLskills.replace("%data%", bio.skills[skill]);
+                $("#skills").append(formattedSkill);
+            }
+        }
+    }
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Sun Yoga Tbilisi",
+            "title" : "Certified Yoga Teacher",
+            "location": "Tbilisi, Georgia",
+            "dates": "2012-2014",
+            "description": "ajdlajkdl aljdal dajlja ljllajdhfioeohgnvbhinkc jeuoeijfo;a;ffheh"
+        },
+        {
+            "employer": "Armenian Branch of SADE",
+            "title" : "Project Manager",
+            "location": "Yerevan, Armenia",
+            "dates": "2009-2011",
+            "description": "djoj niihdwi djfwowej fkhfiwhfow jfoisja;hf fhhohindn a "
+        },
+        {
+            "employer": "Accenture Hungary",
+            "title" : "Consultant",
+            "location": "Budapest, Hungary",
+            "dates": "1997-2008",
+            "description": "djojdodaju ojedfwoj askfoaknfaenfao kafowfjaqojfqo sfasshfos khfia kaf kabfksn ikabfla "
+        }
+    ],
+
+    "display" : function () 
+    {
+        for (job in work.jobs)
+        {
+        
+            $("#workExperience").append(HTMLworkStart);
+
+            var formattedEmployer =
+                HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+            var formattedTitle =
+                HTMLworkTitle.replace("%data%", work.jobs[job].title);
+            var formattedEmployerTitle =
+                formattedEmployer + formattedTitle;
+
+            $(".work-entry:last").append(formattedEmployerTitle);  
+
+            var formattedLocation = 
+                HTMLworkLocation.replace("%data%", work.jobs[job].location);
+
+            $(".work-entry:last").append(formattedLocation); 
+
+            var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+
+            $(".work-entry:last").append(formattedWorkDates);
+
+            var formattedWorkDescr = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+            $(".work-entry:last").append(formattedWorkDescr);
+        }
+    }
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "saa",
+            "dates": "2012-2014",
+            "description": "jk;;;;k;k; ksdbxodjcfl jlakfhka ccnkjhfck  ahljq;d khrqke; ",
+            "images" : ""
+        },
+        {
+            "title": "asdjaldj",
+            "dates": "2012-2014",
+            "description": "jk;;;;k;k; ksdbxodjcfl jlakfhka ccnkjhfck  ahljq;d khrqke; ",
+            "images" : ""
+            
+        }
+    ],
+
+    "display" : function () 
+        {
+            for (project in projects.projects) 
+            {
+        
+                $("#projects").append(HTMLprojectStart);
+
+                var formattedTitle  = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+
+                $(".project-entry:last").append(formattedTitle);
+
+                var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+
+                $(".project-entry:last").append(formattedProjectDates);
+
+                var formattedProjectDescr = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+
+                $(".project-entry:last").append(formattedProjectDescr);
+
+                if (projects.projects[project].images.length > 0)
+                {
+                    for (image in projects.projects[project].images) 
+                    {
+                        var formattedProjectPic = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+                        $(".project-entry:last").append(formattedProjectPic)
+                    }
+                }
+            }
+        }
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "Universite de Picardie",
+            "location": "Budapest, Hungary",
+            "degree": "BA",
+            "majors": "Economics and Business Administration",
+            "dates": "1992-1996",
+            "url": "xxx"
+        }
+    ],
+    
+    "onlineCourses": [
+        {
+            "title": "Front End Web Developer",
+            "school": "Udacity",
+            "date": "2015",
+            "url": "http://www.udacity.com/course/udxxx"
+        }
+    ],
+
+    "display" : function() 
+        {
+            for (school in education.schools) 
+              {
+                
+                $("#education").append(HTMLschoolStart);
+        
+                var formattedSchool = 
+                    HTMLschoolName.replace("%data%", education.schools[school].name);
+                var formattedDegree = 
+                    HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+                var formattedSchoolDegree = formattedSchool + formattedDegree;
+        
+                $(".education-entry:last").append(formattedSchoolDegree);  
+        
+                var formattedLocation = 
+                    HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        
+                $(".education-entry:last").append(formattedLocation); 
+                
+                var formattedDates = 
+                    HTMLschoolDates.replace("%data%", education.schools[school].dates);
+
+                $(".education-entry:last").append(formattedDates);
+
+                var formattedMajors = 
+                    HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+
+                $(".education-entry:last").append(formattedMajors)
+              }
+            for (course in education.onlineCourses) 
+              {
+                $(".education-entry:last").append(HTMLonlineClasses);
+                
+                var formattedTitle = 
+                    HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+
+                var formattedSchool = 
+                    HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+                
+                var formattedTitleSchool = formattedTitle + formattedSchool;
+                
+                $(".education-entry:last").append(formattedTitleSchool);
+                
+                var formattedDates = 
+                    HTMLonlineDates.replace("%data%", education.onlineCourses[course].date);
+                
+                $(".education-entry:last").append(formattedDates);
+                
+                var formattedUrl = HTMLonlineURL.replace("%data%",education.onlineCourses[course].url)
+                
+                $(".education-entry:last").append(formattedUrl)
+            }
+        }
+}
+
+bio.display();
+work.display();
+$(".work-entry:last").append(HTMLgoToTop);
+projects.display();
+$(".project-entry:last").append(HTMLgoToTop);
+education.display();
+$(".education-entry:last").append(HTMLgoToTop);
+
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+    logClicks(x,y);
+});
+
+function locationizer(work_obj) {
+    var locationArray = [];
+    
+    for (job in work_obj.jobs) {
+        var newLocation = work_obj.jobs[job].location;
+        locationArray.push(newLocation);
+    }
+    
+    return locationArray;
+}
+//console.log(locationizer(work));
+//
+function nameChanger(oldName) {
+    var finalName = oldName;
+    var names = oldName.trim().split(" ");
+    names[1] = names[1].toUpperCase();
+    names[0] = names[0].slice(0, 1).toUpperCase() + names[0].slice(1).toLowerCase();
+    finalName = names.join(" ");
+    return finalName;
+}
+
+function inName(name) {
+    if (name === undefined) {
+          name = bio.name;
+    } 
+    return nameChanger(name);
+}
+//
+////console.log(inName("seb hug"));
+//
+
+$('title').text(bio.name);
+//to see a map
+$("#mapDiv").append(googleMap);
